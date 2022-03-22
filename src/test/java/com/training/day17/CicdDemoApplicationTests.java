22588782c4a6b90a6d97afd50623e71281ab64bb
Banupriya S <banupriyas@altimetrik.com>
2022-03-22 15:33:18 +0530

    Commiting CICDDemo project

---
+++ src/test/java/com/training/day17/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.day17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
